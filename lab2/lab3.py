4b4255ff93af8e8572f7c4f7f82c6dfce362b742
mathan761 <kraisenbatya@gmail.com>
2025-09-15 01:48:53 +0500

    Восстановление папок pic и lab2

---
+++ lab2/lab3.py
@@ -0,0 +1,9 @@
+print('Hello World!')
+
+world = 'World'
+print(f"Hello {world}!")
+
+one = 'Hello'
+two = ' '
+three = 'World!'
+print(one + two + three)
---
+++ lab2/lab4.py
@@ -0,0 +1,8 @@
+one = 'some string'
+print(bool(one))
+
+two = 324
+print(float(two))
+
+three = 45.3
+print(str(three))
